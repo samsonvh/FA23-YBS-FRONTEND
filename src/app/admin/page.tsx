931bdca76e1d36ef3431afbae79de2cb1a19925e
SamsonVH <samson.vh.20@gmail.com>
2023-10-06 17:52:14 +0700

    Rebase

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const admin_page = () => {
+  return (
+    <div id="main-content" className="h-screen">admin page</div>
+  )
+}
+
+export default admin_page
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div id="navbar-background" className="bg-[#0B4D8F] h-screen">
+      <div id="navbar-content">
+        <div id="logo-container" className="max-h-12">
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
